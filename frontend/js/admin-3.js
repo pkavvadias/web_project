38173a6572f880ab7e9813914a347cb779376e3b
LabrosAndroutsos <labrosandroutsos.@gmail.com>
2021-02-13 16:04:49 +0200

    histogram stuff

--- frontend/js/admin-3.js
+++ frontend/js/admin-3.js
@@ -37,7 +37,6 @@ function HTTPAnalysis() {
                         label: 'TTL',
 
                         data: valuesTTL,
-                        fill: false,
                         backgroundColor: ["rgb(255, 99, 132)"],
                         borderColor: ["rgb(255, 99, 132)"],
                         borderWidth: 1
@@ -49,13 +48,13 @@ function HTTPAnalysis() {
                             display: false,
                             barPercentage: 1.3,
                             ticks: {
-                                max: valuesTTL.length - 1,
+                                max: 400,
                             }
                         }, {
                             display: true,
                             ticks: {
                                 autoSkip: false,
-                                max: labelsTTL.length - 1,
+                                max: 400,
                             }
                         }],
                         yAxes: [{
@@ -65,94 +64,44 @@ function HTTPAnalysis() {
                         }]
                     }
                 }
-                // Calculating bucket range
-                /* bucket_range = (Math.max.apply(null, valuesTTL) - Math.min.apply(null, valuesTTL)) / 10;
-                 console.log(bucket_range)
 
-                 var Combined = new Array();
-                 Combined[0] = ['Content-Type', 'Time'];
-                 for (var i = 0; i < labelsTTL.length; i++) {
-                     Combined[i + 1] = [labelsTTL[i], valuesTTL[i]];
-                 }*/
 
+                /*var Combined = new Array();
+            Combined[0] = ['Content-Type', 'Time'];
+            for (var i = 0; i < valuesTTL.length; i++) {
+                Combined[i + 1] = [valuesTTL[i], labelsTTL[i]];
+            }
+            console.log([Combined])
+            google.charts.load("current", { packages: ["corechart"] });
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable(Combined);
+
+
+                var options = {
+                    title: 'Content Type Histogram',
+                    legend: { position: 'none' },
+                    bar: { groupWidth: "100%" },
+                    histogram: { maxNumBuckets: 10 }
+                };
+
+                var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
+                chart.draw(data, options);
+            }
+
+*/
 
 
             })
+
         }
     }
     xhr.send();
 }
 
 
-function flow() {
-    var xhr = new XMLHttpRequest();
 
 
-    xhr.open("GET", "http://localhost:3000/getserverips", true);
-
-    //xhr.setRequestHeader('Content-Type', 'json');
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var x = xhr.response;
-            var ips = x.filter(function(e) {
-                if (e.serveripaddress == "") {
-                    return false;
-                }
-                return true;
-            }).map(function(e) {
-
-                return e.serveripaddress;
-
-
-            });
-
-            var ips_count = x.filter(function(e) {
-                if (e.serveripaddress == "") {
-                    return false;
-                }
-                return true;
-            }).map(function(e) {
-                return e.count;
-            });
-
-
-            var xhr_new = [];
-            var data_obj = {};
-            for (i in ips) {
-                (function(i) {
-
-                    xhr_new[i] = new XMLHttpRequest();
-                    url = 'https://get.geojs.io/v1/ip/geo/' + ips[i] + '.json';
-                    xhr_new[i].open("GET", url, false);
-
-                    xhr_new[i].onreadystatechange = function() {
-                        if (xhr_new[i].readyState === 4 && xhr_new[i].status === 200) {
-
-                            res = JSON.parse(xhr_new[i].responseText);
-                            data_obj[i] = {
-                                lat: parseFloat(res.latitude),
-                                long: parseFloat(res.longitude),
-                                count: parseFloat(ips_count[i])
-                            };
-                        }
-                    }
-
-                })(i);
-
-                xhr_new[i].send()
-            }
-
-            console.log(data_obj)
-
-            var addressPoints = [];
-            for (i in data_obj) {
-                addressPoints[i] = [data_obj[i].lat, data_obj[i].long, data_obj[i].count]
-            }
-
-        }
-    }
-}
 document.addEventListener('DOMContentLoaded', HTTPAnalysis);
 //document.addEventListener('DOMContentLoaded', flow);
