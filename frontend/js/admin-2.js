bd4920562193560fb3d25919249bdc17c3adc2a8
Panagiotis Kavadias <p.kavvadias@upnet.gr>
2021-02-13 16:12:44 +0200

    Admin 2(WIP)

--- frontend/js/admin-2.js
+++ frontend/js/admin-2.js
@@ -1,4 +1,5 @@
-function ResponseTimes() {
+
+function ResponseTime() {
 
     var xhr = new XMLHttpRequest();
 
@@ -121,4 +122,110 @@ function ResponseTimes() {
         }
         xhr.send();
     }
+    //document.addEventListener('DOMContentLoaded', ResponseTimes)
     document.addEventListener('DOMContentLoaded', ResponseTimes)
+
+function ResponseTimes() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "http://localhost:3000/getresponsetimes", false);
+
+    //xhr.setRequestHeader('Content-Type', 'json');
+    //xhr.responseType = 'json';
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var x = xhr.response;
+            var data = JSON.parse(x)
+            console.log(data)
+            //var labelsType = data.content_type.map(a => a.content_type)
+            var labelsType = new Array();
+            var days = new Array();
+            var isps = new Array();
+            var methods = new Array();
+            for (x in data) {
+                labelsType.push(data[x].content_type)
+                y = new Date(data[x].starteddatetime)
+                var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                var whichday = day[y.getDay()]
+                days.push(whichday)
+                isps.push(data[x].isp)
+                methods.push(data[x].method)
+            }
+            var labelsType = [...new Set(labelsType)]
+            var days = [...new Set(days)]
+            var isps = [...new Set(isps)]
+            var methods = [...new Set(methods)]
+            for (x in labelsType) {
+                selectTypes = document.getElementById("ctype");
+                var opt = document.createElement("option");
+                opt.text = labelsType[x];
+                selectTypes.add(opt);
+            }
+            for (x in days) {
+                selectTypes = document.getElementById("dates");
+                var opt = document.createElement("option");
+                opt.text = days[x];
+                selectTypes.add(opt);
+            }
+            for (x in isps) {
+                selectTypes = document.getElementById("isp");
+                var opt = document.createElement("option");
+                opt.text = isps[x];
+                selectTypes.add(opt);
+            }
+            for (x in methods) {
+                selectTypes = document.getElementById("methods");
+                var opt = document.createElement("option");
+                opt.text = methods[x];
+                selectTypes.add(opt);
+            }            
+            var color = Chart.helpers.color;
+            var barChartData = {
+                labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00',
+                    '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
+                datasets: []
+            };
+            var ctx = document.getElementById('myChart6').getContext('2d');
+            window.myBar = new Chart(ctx, {
+                type: 'bar',
+                data: barChartData,
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
+                        },
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true,
+                                }
+                            }]
+                        },
+                        title: {
+                            display: true,
+                            text: 'Response Times'
+                        }
+                    }
+                }
+            });
+            $('select').selectpicker();
+            $('#ctype,#dates,#methods,#isp').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
+                var ctypeValues = $('#ctype').val();
+                var datesValues = $('#dates').val();
+                var methodsValues = $('#methods').val();
+                var ispValues = $('#isps').val();
+                updateChart(window.mybar,barChartData,data, ctypeValues, datesValues, methodsValues, ispValues);
+            });
+
+        };
+
+    };
+
+    xhr.send();
+}
+
+function updateChart(chart,data,dataset,ctype,dates,methods,isps){
+    
+}
